2572424c552cb4b11863490f49ecd0fe5cb283a7
63070158 <71114538+63070158@users.noreply.github.com>
2021-05-23 14:09:42 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// time set loader page
+setTimeout(function(){
+  $('.loader_bg').fadeToggle()
+}, 3000);
+
+
+
+
 var w = window.innerWidth;
 var h = window.innerHeight;
 console.log(w,h);
@@ -7,11 +15,7 @@ window.addEventListener('resize', function(event){
     document.querySelector('#page3').style.height = (h/5.581-20.015230245)+'vw';
     document.querySelector('#page4').style.height = (h/3.5573-23.00087145)+'vw';
 });
-// time set loader page
-setTimeout(function(){
-  $('.loader_bg').fadeToggle()
-}, 3000);
-////////////
+
 
 var num = 1;
 function side_slide(event){showImg(num += event);}
@@ -21,11 +25,12 @@ function showImg(event){
     const img = document.querySelectorAll(`.image-carousel`);
     const text = document.querySelectorAll(`#text-carousel`);
     
-    
-    if(event > img.length){num = 1}
+    // num = index value
+    if(event > img.length){num = 1;}
     if(event < 1){num = img.length}
+    console.log("there are "+ img.length + " pic in this slider"); //check how many pic
     for(i = 0; i < img.length; i++){
-        img[i].style.right = '0';
+        // img[i].style.right = '0';
         img[i].style.opacity = '0';
         text[i].style.opacity = '0';
     }
@@ -42,8 +47,10 @@ function showImg(event){
         img[num-1].style.opacity = '1';
         img[num-1].style.right = 35+'%';
     }
+    
     text[num-1].style.opacity = '1';
 }
+
 
 window.addEventListener('scroll', reveal);
 reveal();
@@ -108,50 +115,7 @@ function dropFunction() {
 // }
 // alert
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-  apiKey: "AIzaSyCTWX8XZ3vuO8j43lx5oK28O6p8nd0yd2I",
-  authDomain: "mt-final-8b4db.firebaseapp.com",
-  projectId: "mt-final-8b4db",
-  storageBucket: "mt-final-8b4db.appspot.com",
-  messagingSenderId: "918072828138",
-  appId: "1:918072828138:web:bf51fcbeee81e195a1f793",
-  measurementId: "G-0GH1DXLL07"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
- 
-var firestore = firebase.firestore();
-const docRef = firestore.doc("samples/txt");
-const outputHeader = document.querySelector("#hotDogOutput");
-const inputTextField = document.querySelector("#latestHotDogStatus");
-const saveButton = document.querySelector("#saveButton");
 
-saveButton.addEventListener("click", function(){
-  const textToSave = inputTextField.value;
-  console.log("I am going to save " + textToSave + " to Firestore");
-  docRef.set({
-      hotDogStatus: textToSave
-  }).then(function(){
-      console.log("Status saved");
-  }).catch(function(error){
-      console.log("Got an error: ", error);
-  });
-})
-
-const loadButton = document.querySelector("#loadButton");
-loadButton.addEventListener("click", function(){
-  docRef.get().then(function(doc){
-      if (doc && doc.exists){
-          const myData = doc.data();
-          outputHeader.innerText = "Status: " + myData.hotDogStatus;
-      }
-  }).catch(function(error){
-      console.log("Got an error: ", error);
-  });
-})
 
 
 
